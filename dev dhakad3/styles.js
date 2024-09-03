27d82857898c4037c3c778f7248f1e9295dd38e7
devdhakad1 <devdhakad1520@gmail.com>
2024-09-03 10:36:18 +0530

    Add files via upload
    push commited

---
+++ dev dhakad3/styles.js
@@ -0,0 +1,32 @@
+// Addition js
+
+var t1=document.querySelector(".t1")
+var t2=document.querySelector(".t2")
+var t3=document.querySelector(".t3")
+var btn=document.querySelector(".btn")
+
+btn.addEventListener("click",function(){
+    var a,b,c;
+    a=parseInt(t1.value);
+    b=parseInt(t2.value);
+    c=a+b;
+    t3.value=c
+})
+
+// ----------------------------->>>>>
+// sidedar js 
+
+var box=document.querySelector(".box")
+var btn=document.querySelector(".btn")
+
+btn.addEventListener("click",function(){
+    if(box.classList.contains('box'))
+    {
+       
+        box.classList.remove('box')
+        box.classList.add('active')
+        btn.value=2
+        console.assert(btn.value)
+    }
+})
+
